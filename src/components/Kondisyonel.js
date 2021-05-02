bf5e05c84eacfb590a326774c0e0b5536c25e056
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-03 02:17:20 +0300

    so if I can figure out how to add new components via onclick, my problems will be solved mostly

--- src/components/Kondisyonel.js
+++ src/components/Kondisyonel.js
@@ -39,42 +39,41 @@ class UserInput extends Component {
       name: e.target.value,
     });
   };
+  personal = () => (
+    <section id="information">
+      <h1>Personal Information</h1>
+      <div>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleName}
+          placeholder="Name"
+          required
+        />
+        <input type="text" placeholder="Last name" required />
+      </div>
+      <div>
+        <input type="email" placeholder="e.g. something@someone.com" required />
+      </div>
+      <div>
+        <input type="tel" placeholder="Phone number" />
+      </div>
+      <div>
+        <input type="text" placeholder="Your LinkedIn here" required />
+      </div>
+      <div>
+        <input type="text" placeholder="Your GitHub here" required />
+      </div>
+      <div>
+        <textarea placeholder="Talk about yourself a bit" />
+      </div>
+    </section>
+  );
 
   render() {
     return (
       <form onChange={this.onTrigger}>
-        <section id="information">
-          <h1>Personal Information</h1>
-          <div>
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleName}
-              placeholder="Name"
-              required
-            />
-            <input type="text" placeholder="Last name" required />
-          </div>
-          <div>
-            <input
-              type="email"
-              placeholder="e.g. something@someone.com"
-              required
-            />
-          </div>
-          <div>
-            <input type="tel" placeholder="Phone number" />
-          </div>
-          <div>
-            <input type="text" placeholder="Your LinkedIn here" required />
-          </div>
-          <div>
-            <input type="text" placeholder="Your GitHub here" required />
-          </div>
-          <div>
-            <textarea placeholder="Talk about yourself a bit" />
-          </div>
-        </section>
+        <this.personal />
         <section id="experience">
           <h1>Experience</h1>
           <div>
