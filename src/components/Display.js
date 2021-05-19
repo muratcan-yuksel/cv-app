59b812aeba0da0d30610f852695f107cfb2cf0c1
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-19 21:42:11 +0300

    I guess I converted the display js ?

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,26 +1,41 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import UserInput from "./UserInput";
 import PreviewCV from "./PreviewCV";
 
-class Display extends Component {
-  constructor(props) {
-    super(props);
+// class Display extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      data: null,
-    };
-  }
-  handleCallback = (childData) => {
-    this.setState({ data: childData });
+//     this.state = {
+//       data: null,
+//     };
+//   }
+//   handleCallback = (childData) => {
+//     this.setState({ data: childData });
+//   };
+//   render() {
+//     const { data } = this.state;
+//     const isFinished = this.props.isFinished;
+//     if (isFinished) {
+//       return <PreviewCV toChild={data} />;
+//     }
+//     return <UserInput parentCallback={this.handleCallback} />;
+//   }
+// }
+
+function Display(props) {
+  const [data, setData] = useState(null);
+
+  const handleCallback = (childData) => {
+    setData({ data: childData });
   };
-  render() {
-    const { data } = this.state;
-    const isFinished = this.props.isFinished;
-    if (isFinished) {
-      return <PreviewCV toChild={data} />;
-    }
-    return <UserInput parentCallback={this.handleCallback} />;
+  const isFinished = props.isFinished;
+
+  if (isFinished) {
+    return <PreviewCV toChild={data} />;
   }
+  return <UserInput parentCallback={handleCallback} />;
 }
 
 export default Display;
