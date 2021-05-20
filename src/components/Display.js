cae9b0abe0ebeeab7612bb285451065894160c6f
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-20 09:00:57 +0300

    trying to convert the display.js, failing bad

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,41 +1,40 @@
-import React, { Component } from "react";
-// import React, { useState } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import UserInput from "./UserInput";
 import PreviewCV from "./PreviewCV";
 
-class Display extends Component {
-  constructor(props) {
-    super(props);
+// class Display extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      data: null,
-    };
-  }
-  handleCallback = (childData) => {
-    this.setState({ data: childData });
-  };
-  render() {
-    const { data } = this.state;
-    const isFinished = this.props.isFinished;
-    if (isFinished) {
-      return <PreviewCV toChild={data} />;
-    }
-    return <UserInput parentCallback={this.handleCallback} />;
-  }
-}
-
-// function Display(props) {
-//   const [count, setCount] = useState({ data: null });
-
-//   const handleCallback = (childData) => {
-//     setCount({ data: childData });
-//   };
-//   const isFinished = props.isFinished;
-
-//   if (isFinished) {
-//     return <PreviewCV toChild={count} />;
+//     this.state = {
+//       data: null,
+//     };
 //   }
-//   return <UserInput parentCallback={handleCallback} />;
+//   handleCallback = (childData) => {
+//     this.setState({ data: childData });
+//   };
+//   render() {
+//     const { data } = this.state;
+//     const isFinished = this.props.isFinished;
+//     if (isFinished) {
+//       return <PreviewCV toChild={data} />;
+//     }
+//     return <UserInput parentCallback={this.handleCallback} />;
+//   }
 // }
+//this does not work, we're gonna need to use useEffect I suppose
+function Display(props) {
+  const [state, setState] = useState({ data: null });
+
+  const handleCallback = (childData) => {
+    setState({ data: childData });
+  };
+  const isFinished = props.isFinished;
+  if (isFinished) {
+    return <PreviewCV toChild={state} />;
+  }
+  return <UserInput parentCallback={handleCallback} />;
+}
 
 export default Display;
