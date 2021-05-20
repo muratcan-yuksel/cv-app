9fab3f4ac7a1205eabf31cc77e6fab8ab6c7e783
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-20 18:12:32 +0300

    gave up trying to convert the userinput.js

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,43 +1,47 @@
-// import React, { Component } from "react";
-import React, { useState } from "react";
+import React, { Component } from "react";
+// import React, { useState } from "react";
 import Informatsion from "./Informatsion";
 import Work from "./Work";
 import Studies from "./Studies";
 import "../styles/UserInput.css";
 
-function UserInput(props) {
-  const [state, setState] = useState({
-    information: [
-      {
-        firstName: "",
-        lastName: "",
-        email: "",
-        tel: "",
-        linkedin: "",
-        github: "",
-        bio: "",
-      },
-    ],
-    work: [
-      {
-        position: "",
-        company: "",
-        jobDescription: "",
-        startDate: "",
-        endDate: "",
-      },
-    ],
-    studies: [
-      {
-        schoolName: "",
-        studyArea: "",
-        schoolStartDate: "",
-        schoolEndDate: "",
-      },
-    ],
-  });
+class UserInput extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleChange = (e) => {
+    this.state = {
+      information: [
+        {
+          firstName: "",
+          lastName: "",
+          email: "",
+          tel: "",
+          linkedin: "",
+          github: "",
+          bio: "",
+        },
+      ],
+      work: [
+        {
+          position: "",
+          company: "",
+          jobDescription: "",
+          startDate: "",
+          endDate: "",
+        },
+      ],
+      studies: [
+        {
+          schoolName: "",
+          studyArea: "",
+          schoolStartDate: "",
+          schoolEndDate: "",
+        },
+      ],
+    };
+  }
+
+  handleChange = (e) => {
     if (
       [
         "position",
@@ -47,17 +51,17 @@ function UserInput(props) {
         "endDate",
       ].includes(e.target.className)
     ) {
-      let work = [...state.work];
+      let work = [...this.state.work];
       work[e.target.dataset.id][e.target.className] = e.target.value;
-      setState({ work });
+      this.setState({ work });
     } else if (
       ["schoolName", "studyArea", "schoolStartDate", "schoolEndDate"].includes(
         e.target.className
       )
     ) {
-      let studies = [...state.studies];
+      let studies = [...this.state.studies];
       studies[e.target.dataset.id][e.target.className] = e.target.value;
-      setState({ studies });
+      this.setState({ studies });
     } else if (
       [
         "firstName",
@@ -69,118 +73,130 @@ function UserInput(props) {
         "bio",
       ].includes(e.target.className)
     ) {
-      let information = [...state.information];
+      let information = [...this.state.information];
       information[e.target.dataset.id][e.target.className] = e.target.value;
-      setState({ information });
+      this.setState({ information });
     } else {
-      setState({ [e.target.name]: e.target.value });
+      this.setState({ [e.target.name]: e.target.value });
     }
   };
-  const addWork = (e) => {
-    setState([
-      ...state.work,
-      {
-        position: "",
-        company: "",
-        jobDescription: "",
-        startDate: "",
-        endDate: "",
-      },
-    ]);
-  };
-  //   addWork = (e) => {
-  //     this.setState((prevState) => ({
-  //       work: [
-  //         ...prevState.work,
-  //         {
-  //           position: "",
-  //           company: "",
-  //           jobDescription: "",
-  //           startDate: "",
-  //           endDate: "",
-  //         },
-  //       ],
-  //     }));
-  //   };
 
-  const addStudies = (e) => {
-    setState([
-      ...state.studies,
-      {
-        schoolName: "",
-        studyArea: "",
-        schoolStartDate: "",
-        schoolEndDate: "",
-      },
-    ]);
+  addWork = (e) => {
+    this.setState((prevState) => ({
+      work: [
+        ...prevState.work,
+        {
+          position: "",
+          company: "",
+          jobDescription: "",
+          startDate: "",
+          endDate: "",
+        },
+      ],
+    }));
   };
-  const onTrigger = (event) => {
-    props.parentCallback(state);
+  addStudies = (e) => {
+    this.setState((prevState) => ({
+      studies: [
+        ...prevState.studies,
+        {
+          schoolName: "",
+          studyArea: "",
+          schoolStartDate: "",
+          schoolEndDate: "",
+        },
+      ],
+    }));
   };
-  const twoCalls = (e) => {
-    onTrigger(e);
-    handleChange(e);
+  onTrigger = (event) => {
+    this.props.parentCallback(this.state);
+  };
+  twoCalls = (e) => {
+    this.onTrigger(e);
+    this.handleChange(e);
     // console.log(this.state);
   };
-  let { work } = state;
-  let { studies } = state;
-  let { information } = state;
-  return (
-    <div className="input" onChange={twoCalls}>
-      <div id="header">CV-maker</div>
-      <Informatsion information={information} />
-      <h1>Experience</h1>
-      <Work work={work} />
-      <button className="button" onClick={addWork}>
-        Add work experience
-      </button>
 
-      <h1>Education</h1>
-      <Studies studies={studies} />
-      <button className="button" onClick={addStudies}>
-        Add education
-      </button>
-    </div>
-  );
+  render() {
+    let { work } = this.state;
+    let { studies } = this.state;
+    let { information } = this.state;
+    return (
+      <div className="input" onChange={this.twoCalls}>
+        <div id="header">CV-maker</div>
+        <Informatsion information={information} />
+        <h1>Experience</h1>
+        <Work work={work} />
+        <button className="button" onClick={this.addWork}>
+          Add work experience
+        </button>
+
+        <h1>Education</h1>
+        <Studies studies={studies} />
+        <button className="button" onClick={this.addStudies}>
+          Add education
+        </button>
+      </div>
+    );
+  }
 }
 
-// class UserInput extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       information: [
-//         {
-//           firstName: "",
-//           lastName: "",
-//           email: "",
-//           tel: "",
-//           linkedin: "",
-//           github: "",
-//           bio: "",
-//         },
-//       ],
-//       work: [
-//         {
-//           position: "",
-//           company: "",
-//           jobDescription: "",
-//           startDate: "",
-//           endDate: "",
-//         },
-//       ],
-//       studies: [
-//         {
-//           schoolName: "",
-//           studyArea: "",
-//           schoolStartDate: "",
-//           schoolEndDate: "",
-//         },
-//       ],
-//     };
-//   }
-
-//   handleChange = (e) => {
+export default UserInput;
+// function UserInput(props) {
+//   const [state, setState] = useState({
+//     information: [
+//       {
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         tel: "",
+//         linkedin: "",
+//         github: "",
+//         bio: "",
+//       },
+//     ],
+//     work: [
+//       {
+//         position: "",
+//         company: "",
+//         jobDescription: "",
+//         startDate: "",
+//         endDate: "",
+//       },
+//     ],
+//     studies: [
+//       {
+//         schoolName: "",
+//         studyArea: "",
+//         schoolStartDate: "",
+//         schoolEndDate: "",
+//       },
+//     ],
+//   });
+//   console.log(state.work);
+//   //   handleChange = (e) => {
+//   //     if (
+//   //       [
+//   //         "position",
+//   //         "company",
+//   //         "jobDescription",
+//   //         "startDate",
+//   //         "endDate",
+//   //       ].includes(e.target.className)
+//   //     ) {
+//   //       let work = [...this.state.work];
+//   //       work[e.target.dataset.id][e.target.className] = e.target.value;
+//   //       this.setState({ work });
+//   //     } else if (
+//   //       ["schoolName", "studyArea", "schoolStartDate", "schoolEndDate"].includes(
+//   //         e.target.className
+//   //       )
+//   //     ) {
+//   //       let studies = [...this.state.studies];
+//   //       studies[e.target.dataset.id][e.target.className] = e.target.value;
+//   //       this.setState({ studies });
+//   //     } else if (
+//   const handleChange = (e) => {
 //     if (
 //       [
 //         "position",
@@ -190,17 +206,17 @@ function UserInput(props) {
 //         "endDate",
 //       ].includes(e.target.className)
 //     ) {
-//       let work = [...this.state.work];
+//       let work = [...state.work];
 //       work[e.target.dataset.id][e.target.className] = e.target.value;
-//       this.setState({ work });
+//       setState({ ...state.work, ...state.work });
 //     } else if (
 //       ["schoolName", "studyArea", "schoolStartDate", "schoolEndDate"].includes(
 //         e.target.className
 //       )
 //     ) {
-//       let studies = [...this.state.studies];
+//       let studies = [...state.studies];
 //       studies[e.target.dataset.id][e.target.className] = e.target.value;
-//       this.setState({ studies });
+//       setState({ studies, ...state.studies });
 //     } else if (
 //       [
 //         "firstName",
@@ -212,72 +228,77 @@ function UserInput(props) {
 //         "bio",
 //       ].includes(e.target.className)
 //     ) {
-//       let information = [...this.state.information];
+//       let information = [...state.information];
 //       information[e.target.dataset.id][e.target.className] = e.target.value;
-//       this.setState({ information });
+//       setState({ information, ...state.information });
 //     } else {
-//       this.setState({ [e.target.name]: e.target.value });
+//       setState({ [e.target.name]: e.target.value });
 //     }
 //   };
+//   const addWork = (e) => {
+//     setState([
+//       ...state.work,
+//       {
+//         position: "",
+//         company: "",
+//         jobDescription: "",
+//         startDate: "",
+//         endDate: "",
+//       },
+//     ]);
+//   };
+//   //   addWork = (e) => {
+//   //     this.setState((prevState) => ({
+//   //       work: [
+//   //         ...prevState.work,
+//   //         {
+//   //           position: "",
+//   //           company: "",
+//   //           jobDescription: "",
+//   //           startDate: "",
+//   //           endDate: "",
+//   //         },
+//   //       ],
+//   //     }));
+//   //   };
 
-//   addWork = (e) => {
-//     this.setState((prevState) => ({
-//       work: [
-//         ...prevState.work,
-//         {
-//           position: "",
-//           company: "",
-//           jobDescription: "",
-//           startDate: "",
-//           endDate: "",
-//         },
-//       ],
-//     }));
+//   const addStudies = (e) => {
+//     setState([
+//       ...state.studies,
+//       {
+//         schoolName: "",
+//         studyArea: "",
+//         schoolStartDate: "",
+//         schoolEndDate: "",
+//       },
+//     ]);
 //   };
-//   addStudies = (e) => {
-//     this.setState((prevState) => ({
-//       studies: [
-//         ...prevState.studies,
-//         {
-//           schoolName: "",
-//           studyArea: "",
-//           schoolStartDate: "",
-//           schoolEndDate: "",
-//         },
-//       ],
-//     }));
+//   const onTrigger = (event) => {
+//     props.parentCallback(state);
 //   };
-//   onTrigger = (event) => {
-//     this.props.parentCallback(this.state);
-//   };
-//   twoCalls = (e) => {
-//     this.onTrigger(e);
-//     this.handleChange(e);
+//   const twoCalls = (e) => {
+//     onTrigger(e);
+//     handleChange(e);
 //     // console.log(this.state);
 //   };
+//   let { work } = state;
+//   let { studies } = state;
+//   let { information } = state;
+//   return (
+//     <div className="input" onChange={twoCalls}>
+//       <div id="header">CV-maker</div>
+//       <Informatsion information={information} />
+//       <h1>Experience</h1>
+//       <Work work={work} />
+//       <button className="button" onClick={addWork}>
+//         Add work experience
+//       </button>
 
-//   render() {
-//     let { work } = this.state;
-//     let { studies } = this.state;
-//     let { information } = this.state;
-//     return (
-//       <div className="input" onChange={this.twoCalls}>
-//         <div id="header">CV-maker</div>
-//         <Informatsion information={information} />
-//         <h1>Experience</h1>
-//         <Work work={work} />
-//         <button className="button" onClick={this.addWork}>
-//           Add work experience
-//         </button>
-
-//         <h1>Education</h1>
-//         <Studies studies={studies} />
-//         <button className="button" onClick={this.addStudies}>
-//           Add education
-//         </button>
-//       </div>
-//     );
-//   }
+//       <h1>Education</h1>
+//       <Studies studies={studies} />
+//       <button className="button" onClick={addStudies}>
+//         Add education
+//       </button>
+//     </div>
+//   );
 // }
-
-export default UserInput;
